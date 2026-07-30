7e514bf4ef351a0f33a890808c8d42c972178032
kota-batu <paradiseuniversetour@gmail.com>
2026-07-30 09:44:59 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,8 @@
  * ================================================================ */
 
 // SAMA PERSIS dengan API_URL di situs PAM Finance (backend dipakai bersama)
-const API_URL = "https://script.google.com/macros/s/AKfycbz87RpwWVy0oRlWnCxxtGQsmd_D_NcDFJ7Zd-nLZcXJs99aulNpMjKQ5v2oEtRf5N8D/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwe4kdR_gje9Uj7ijf_CVURCx66Z6Ani_QeWXz7djAYLBfYIJFoVfmK0f6JIDyPWP6w/exec";
+
 
 // GANTI dengan URL situs PAM Finance utama kamu (dipakai untuk generate link QR
 // supaya QR bisa discan pakai kamera HP APAPUN, tidak harus di dalam situs ini)
