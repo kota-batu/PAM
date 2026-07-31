31267b3982dbc199c66f33e62570b99d5207e3e4
kota-batu <paradiseuniversetour@gmail.com>
2026-07-31 13:44:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  * ================================================================ */
 
 // SAMA PERSIS dengan API_URL di situs PAM Finance (backend dipakai bersama)
-const API_URL = "https://script.google.com/macros/s/AKfycbwkKpTsQyktiztC8_dHZULOcgWwi_nF2uBIq8NsIDlpbvnLRqnaebN7h7EXjJN55GcL/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw0CbZIrDgG84rqkeVNNhd-0QN1fACUSemrsFid04Fp4fMJ5CXo9pwM1iIJU6ng5GYq/exec";
 
 
 // GANTI dengan URL situs PAM Finance utama kamu (dipakai untuk generate link QR
